756b7e89c45233e6cd18b28e65534d27c6ac89a7
powr <powr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-29 19:51:53 +0000

    Updates
    git-svn-id: https://plugins.svn.wordpress.org/powr-tabs/trunk@1170483 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- powr-tabs.php
+++ powr-tabs.php
@@ -1,14 +1,14 @@
 <?php
     /**
      * @package POWr Tabs
-     * @version 1.4
+     * @version 1.4.001
      */
     /*
     Plugin Name: POWr Tabs
     Plugin URI: http://www.powr.io
     Description: Organize and display any content with custom navigation tabs!  Drop the widget anywhere in your theme. Or use the POWr icon in your WP text editor to add to a page or post. Edit on your live page by clicking the settings icon. More plugins and tutorials at POWr.io.
     Author: POWr.io
-    Version: 1.4
+    Version: 1.4.001
     Author URI: http://www.powr.io
     */
 
